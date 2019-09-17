d6814619cb00219d5a10f2297a864fd1e4cf5377
Zahirgeek <zahirgeek@gmail.com>
2019-09-18 01:41:58 +0800

    convert lrc

---
+++ README.md
@@ -0,0 +1,20 @@
+# transcode_utf8操作说明
+> 这是一款将文本文件编码格式批量转换为utf-8的小程序
+
+> 因为是为了解决蛋疼的播放器不支持utf-8以外格式的歌词文件，所以目前仅支持转换.lrc文件，其他文件暂不支持
+
+> 该程序在python3.6环境下开发，运行需要python3.6支持
+
+> 运行前请先安装python3.6或以上版本
+
+> 默认输出目录为用户指定目录中的transform目录中
+
+> 运行前需安装模块
+```
+pip install -r requirements.txt 
+```
+
+## 运行程序
+```
+python transcode.py
+```
---
+++ transcode.py
@@ -0,0 +1,88 @@
+import os
+import codecs
+import chardet
+
+# 将文件转换成UTF-8编码
+def convert(file_name, file_path, in_code="GBK", out_code="UTF-8"):
+    '''
+    :param file_name: 文件名
+    :param file_path: 文件路径
+    :param in_code: 输入文件格式，默认GBK
+    :param out_code: 输出文件格式，默认UTF-8
+    :return:
+    '''
+
+    # 文件位置
+    file = os.path.join(file_path, file_name)
+    try:
+        with codecs.open(file, "rb", in_code) as f_in:
+            new_content = f_in.read()
+            out_path = "transform"
+            file_out = os.path.join(file_path, out_path)
+            isExists = os.path.exists(file_out)
+            if not isExists:
+                os.makedirs(file_out)
+
+            file_out = os.path.join(file_out, file_name)
+            # 转换后文件默认保存在当前目录的transform目录中
+            with codecs.open(file_out, "w", out_code) as f_out:
+                f_out.write(new_content)
+
+    except IOError as err:
+        print("I/O error: {}".format(err))
+
+# 返回路径该级所有文件名和文件路径
+def list_folders_files(path):
+    '''
+    :param path: 输入的文件路径
+    :return:
+    '''
+
+    # 文件夹路径列表
+    list_folders = []
+    # 文件路径列表
+    list_files = []
+    for file in os.listdir(path):
+        file_path = os.path.join(path, file)
+        if os.path.isdir(file_path):
+            list_folders.append(file_path)
+        else:
+            list_files.append(file)
+
+    return list_files, list_folders
+
+#遍历输入文件路径下所有文件和路径并执行转换
+def transform(path):
+
+    list_file_name, list_folder_name = list_folders_files(path)
+    current_path = path
+
+    # 批量转换当前路径下的文件
+    for file_name in list_file_name:
+        file_path = os.path.join(current_path, file_name)
+        with open(file_path, 'rb') as f_in:
+            data = f_in.read()
+            code_type = chardet.detect(data)['encoding']
+        if code_type == 'GB2312' or 'gb2312':
+            convert(file_name, current_path)
+        else:
+            convert(file_name, current_path, in_code=code_type)
+
+# 判断用户输入, 返回转换目录
+def user_input():
+
+    file_path = input(">>请输入你需要转换的文件所在目录，默认为该程序当前目录,转换后的文件在该目录的transform文件夹下\n"
+                      ">>Please input your convert directory, the default directory is current directory of this program,"
+                      "the converted file is under the transform folder in current directory\n")
+    if not file_path:
+        file_path = os.path.abspath('transcode.py')
+        file_path = file_path.split("\\")[:-1]
+        file_path = "\\".join(file_path)
+    else:
+        if not os.path.exists(file_path):
+            print("不存在该目录，程序退出")
+    return file_path
+
+if __name__ == '__main__':
+    file_path = user_input()
+    transform(file_path)
