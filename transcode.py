4e08aeed6522ee5b9e6d66ef9d3feff622b2a1d6
Zahirgeek <zahirgeek@gmail.com>
2019-09-19 19:41:57 +0800

    fix utf8 bug

--- transcode.py
+++ transcode.py
@@ -2,6 +2,7 @@ import os
 import codecs
 import chardet
 
+
 # 将文件转换成UTF-8编码
 def convert(file_name, file_path, in_code="GBK", out_code="UTF-8"):
     '''
@@ -11,7 +12,8 @@ def convert(file_name, file_path, in_code="GBK", out_code="UTF-8"):
     :param out_code: 输出文件格式，默认UTF-8
     :return:
     '''
-
+    if in_code == "UTF-8":
+        return 0
     # 文件位置
     file = os.path.join(file_path, file_name)
     try:
@@ -30,6 +32,7 @@ def convert(file_name, file_path, in_code="GBK", out_code="UTF-8"):
 
     except IOError as err:
         print("I/O error: {}".format(err))
+
 
 # 返回路径该级所有文件名和文件路径
 def list_folders_files(path):
@@ -53,7 +56,8 @@ def list_folders_files(path):
 
     return list_files, list_folders
 
-#遍历输入文件路径下所有文件和路径并执行转换
+
+# 遍历输入文件路径下所有文件和路径并执行转换
 def transform(path):
 
     list_file_name, list_folder_name = list_folders_files(path)
@@ -65,10 +69,15 @@ def transform(path):
         with open(file_path, 'rb') as f_in:
             data = f_in.read()
             code_type = chardet.detect(data)['encoding']
-        if code_type == 'GB2312' or 'gb2312':
+            code_type = code_type.upper()
+
+        if code_type == 'UTF-8':
+            continue
+        if code_type == 'GB2312':
             convert(file_name, current_path)
         else:
             convert(file_name, current_path, in_code=code_type)
+
 
 # 判断用户输入, 返回转换目录
 def user_input():
@@ -76,6 +85,7 @@ def user_input():
     file_path = input(">>请输入你需要转换的文件所在目录，默认为该程序当前目录,转换后的文件在该目录的transform文件夹下\n"
                       ">>Please input your convert directory, the default directory is current directory of this program,"
                       "the converted file is under the transform folder in current directory\n")
+
     if not file_path:
         file_path = os.path.abspath('transcode.py')
         file_path = file_path.split("\\")[:-1]
@@ -85,6 +95,7 @@ def user_input():
             print("不存在该目录，程序退出")
 
     return file_path
+
 
 if __name__ == '__main__':
     file_path = user_input()
